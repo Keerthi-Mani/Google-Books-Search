5a11e42cb64d7d22bc85ec816d462ce8b9054357
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-26 19:07:13 -0500

    Search Books from Google

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+  <footer className="page-footer">
+    <div className="container-fluid text-center">
+      <p>Keerthi Mani</p>
+    </div>
+  </footer>
+);
+export default Footer;
